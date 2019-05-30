f6d50ba41753876f74dd73fd0d8c0bff3c34dabf
Nikhil Thomas <nikhilthomas1@gmail.com>
2019-05-30 16:59:42 +0530

    Improve test structure
    Improve test code structure following
    https://github.com/operator-framework/operator-marketplace/blob/master/docs/e2e-testing.md
    
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ test/helpers/helpers.go
@@ -0,0 +1,11 @@
+package helpers
+
+import "testing"
+
+// AssertNoError confirms the error returned is nil
+func AssertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
