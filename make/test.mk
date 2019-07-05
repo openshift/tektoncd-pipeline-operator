a35c103411f26e156d8cf67ee1a3495f50221e4c
Nikhil Thomas <nikhilthomas1@gmail.com>
2019-07-05 12:20:11 +0530

    Update email ENV variable in image build step
    Set maintainer and email to pipelines-dev@redhat.com
    
    Update test-e2e target with verbose go test flag
    
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>
    
    Update maintainer email
    
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

--- make/test.mk
+++ make/test.mk
@@ -47,7 +47,7 @@ test-e2e: ./vendor e2e-setup
 ifeq ($(OPENSHIFT_VERSION),3)
 	$(Q)oc login -u system:admin
 endif
-	$(Q)operator-sdk test local ./test/e2e --image registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:tektoncd-pipeline-operator --namespace $(TEST_NAMESPACE) --verbose --debug
+	$(Q)operator-sdk test local ./test/e2e --image registry.svc.ci.openshift.org/${OPENSHIFT_BUILD_NAMESPACE}/stable:tektoncd-pipeline-operator --namespace $(TEST_NAMESPACE) --debug --go-test-flags "-v -timeout=15m"
 
 .PHONY: e2e-setup
 e2e-setup: e2e-cleanup 
