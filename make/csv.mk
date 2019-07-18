2b8c46c078339e03260f061330d7aeb173b0309b
Nikhil Thomas <nikhilthomas1@gmail.com>
2019-07-18 17:10:04 +0530

    Add Make targets for generating CSV and operator source
    Add a CSV, package template in ./manifests
    
    Add `opertorsource` template in ./deploy/install
    
    Add Make Targets
        `tag-image`          : tag image built by operator-sdk or CI
        `gen-csv`            : render csv template in ./manifests uisng the tagged image and other appropriate params
        `push-quay-app`      : push bundle (csv, package, crd) to Quay (Quay Application)
        `gen-operator-source`: generate an `operatorsource` for local testing using the bundle pushed to Quay
    
    Signed-off-by: Nikhil Thomas <nikhilthomas1@gmail.com>

---
+++ make/csv.mk
@@ -0,0 +1,47 @@
+QUAY_USERNAME ?=
+QUAY_PASSWORD ?=
+
+OPERATOR_NAME ?= openshift-pipelines-operator
+OPERATOR_VERSION ?= 0.5.0
+
+QYAPP_NAMESPACE ?= openshift-pipeline
+QYAPP_REPOSITORY ?= openshift-pipelines-operators
+
+OPERATOR_CI_IMAGE ?=
+OPERATOR_IMAGE ?= quay.io/$(QYAPP_NAMESPACE)/$(OPERATOR_NAME)
+
+INSTALL_DIR ?= deploy/install
+
+.PHONY: gen-tag
+gen-tag:
+	$(eval export TAG := $(shell date +%s))
+
+.PHONY: gen-tag
+tag-image: gen-tag
+	docker tag $(OPERATOR_CI_IMAGE) $(OPERATOR_IMAGE):$(OPERATOR_VERSION)-$(TAG)
+
+
+.PHONY: gen-csv
+gen-csv: tag-image
+	$(eval OPERATOR_MANIFESTS := ./tmp/openshift-pipelines-operator)
+	$(eval CREATION_TIMESTAMP := $(shell date --date="@$(TAG)" '+%Y-%m-%d %H:%M:%S'))
+	operator-courier --verbose flatten manifests/ $(OPERATOR_MANIFESTS)
+	cp -vf deploy/crds/*_crd.yaml $(OPERATOR_MANIFESTS)
+	@sed -i -e 's,REPLACE_NAME,$(OPERATOR_NAME),g' $(OPERATOR_MANIFESTS)/openshift-pipelines-operator.v$(OPERATOR_VERSION).clusterserviceversion-v$(OPERATOR_VERSION).yaml
+	@sed -i -e 's,REPLACE_VERSION,$(OPERATOR_VERSION),g' $(OPERATOR_MANIFESTS)/openshift-pipelines-operator.v$(OPERATOR_VERSION).clusterserviceversion-v$(OPERATOR_VERSION).yaml
+	@sed -i -e 's,REPLACE_IMAGE,$(OPERATOR_IMAGE):$(OPERATOR_VERSION)-$(TAG),g' $(OPERATOR_MANIFESTS)/openshift-pipelines-operator.v$(OPERATOR_VERSION).clusterserviceversion-v$(OPERATOR_VERSION).yaml
+	@sed -i -e 's,REPLACE_CREATED_AT,$(CREATION_TIMESTAMP),' $(OPERATOR_MANIFESTS)/openshift-pipelines-operator.v$(OPERATOR_VERSION).clusterserviceversion-v$(OPERATOR_VERSION).yaml
+	@sed -i -e 's,REPLACE_NAME,$(OPERATOR_NAME),g' $(OPERATOR_MANIFESTS)/openshift-pipelines-operator.package.yaml
+	@sed -i -e 's,REPLACE_VERSION,$(OPERATOR_VERSION),g' $(OPERATOR_MANIFESTS)/openshift-pipelines-operator.package.yaml
+	@sed -i -e 's,REPLACE_PACKAGE,$(QYAPP_REPOSITORY),' $(OPERATOR_MANIFESTS)/openshift-pipelines-operator.package.yaml
+	operator-courier --verbose verify --ui_validate_io $(OPERATOR_MANIFESTS)
+
+push-quay-app: gen-csv
+	$(eval QUAY_API_TOKEN := $(shell curl -sH "Content-Type: application/json" -XPOST https://quay.io/cnr/api/v1/users/login -d '{"user":{"username":"'${QUAY_USERNAME}'","password":"'${QUAY_PASSWORD}'"}}' | jq -r '.token'))
+	@operator-courier push $(OPERATOR_MANIFESTS) $(QYAPP_NAMESPACE) $(QYAPP_REPOSITORY) $(OPERATOR_VERSION)-$(TAG) "$(QUAY_API_TOKEN)"
+
+.PHONY: gen-operator-source
+gen-operator-source: push-quay-app
+	cp $(INSTALL_DIR)/operator-source.yaml ./tmp/
+	@sed -i -e 's,REPLACE_NAMESPACE,$(QYAPP_NAMESPACE),g' ./tmp/operator-source.yaml
+	@sed -i -e 's,REPLACE_REPOSITORY,$(QYAPP_REPOSITORY),g' ./tmp/operator-source.yaml
