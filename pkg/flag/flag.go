a3f1ef3d426053e53ecb11072ee2b9e2d2720299
Piyush Garg <piyushgarg001@gmail.com>
2020-08-17 12:59:57 +0530

    :Add payload: pipelines,clustertasks,triggers,consolesampleyamls

--- pkg/flag/flag.go
+++ pkg/flag/flag.go
@@ -44,7 +44,7 @@ const (
 var (
 	flagSet *pflag.FlagSet
 
-	TektonVersion          = "devel"
+	TektonVersion          = "release-next"
 	PipelineSA             string
 	IgnorePattern          string
 	ResourceWatched        string
