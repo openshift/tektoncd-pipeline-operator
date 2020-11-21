ad43d85c42b3f6472890baa7d828f868d19c5d8a
Alan Field <afield@redhat.com>
2020-11-21 00:08:59 +0000

    :Add nightly or last release pipeline manifest

--- pkg/flag/flag.go
+++ pkg/flag/flag.go
@@ -60,7 +60,7 @@ const (
 var (
 	flagSet *pflag.FlagSet
 
-	TektonVersion          = "devel"
+	TektonVersion          = "release-next"
 	PipelineSA             string
 	IgnorePattern          string
 	ResourceWatched        string
